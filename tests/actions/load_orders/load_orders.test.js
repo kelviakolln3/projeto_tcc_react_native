a1108fd4e02c6991893d2726fe57a63b0698ed36
kelvia_kolln <kelviakolln19@hotmail.com>
2024-10-10 16:24:20 -0300

    load orders

---
+++ tests/actions/load_orders/load_orders.test.js
@@ -0,0 +1,99 @@
+import configureMockStore from 'redux-mock-store';
+import { thunk } from 'redux-thunk';
+import axios from 'axios';
+import { fetchData } from '../../../src/actions/orders/ordersLoadAction'; 
+import { FETCH_DATA_REQUEST, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE } from '../../../src/actions/orders/ordersLoadActionTypes';
+import { faker } from '@faker-js/faker';
+
+const mockStore = configureMockStore([thunk]);
+
+jest.mock('axios');
+
+describe('fetchData actions', () => { //Tempo de montagem 04 min e 31 seg 
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks(); // Limpa os mocks após cada teste
+  });
+
+  it('should dispatch FETCH_DATA_REQUEST and FETCH_DATA_SUCCESS when fetching data is successful', async () => {
+    const mockData = [
+        {
+            idPedido: faker.number.int(),
+            idCliente: faker.number.int(),
+            idUsuario: faker.number.int(),
+            dataCriacao: faker.date.recent().toISOString(),
+            condicaoPagamento: faker.string.alpha(10),
+            formaPagamento: faker.string.alpha(10),
+            total: faker.number.float(),
+            itemPedidoBeans: [
+                {
+                    idItemPedido: faker.number.int(),
+                    idProduto: faker.number.int(),
+                    quantidade: faker.number.float(),
+                    valorUnitario: faker.number.float(),
+                }
+            ]
+        },
+        {
+            idPedido: faker.number.int(),
+            idCliente: faker.number.int(),
+            idUsuario: faker.number.int(),
+            dataCriacao: faker.date.recent().toISOString(),
+            condicaoPagamento: faker.string.alpha(10),
+            formaPagamento: faker.string.alpha(10),
+            total: faker.number.float(),
+            itemPedidoBeans: [
+                {
+                    idItemPedido: faker.number.int(),
+                    idProduto: faker.number.int(),
+                    quantidade: faker.number.float(),
+                    valorUnitario: faker.number.float(),
+                }
+            ]
+        },
+    ];
+
+    const fakeUrl = 'http://fakeurl.com/api/pedido/get-all-bean';
+    
+    // Simula uma resposta bem-sucedida
+    axios.get.mockResolvedValueOnce({ status: 200, data: mockData });
+
+    // Despacha a ação fetchData
+    await store.dispatch(fetchData(fakeUrl));
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: FETCH_DATA_REQUEST });
+    expect(actions[1]).toEqual({ type: FETCH_DATA_SUCCESS, payload: mockData });
+  });
+
+  it('should dispatch FETCH_DATA_REQUEST and FETCH_DATA_FAILURE when fetching data fails', async () => {
+    const errorMessage = 'Network error';
+
+    // Simula um erro na requisição
+    axios.get.mockRejectedValueOnce(new Error(errorMessage));
+
+    // Despacha a ação fetchData
+    await store.dispatch(fetchData());
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: FETCH_DATA_REQUEST });
+    expect(actions[1]).toEqual({ type: FETCH_DATA_FAILURE, error: errorMessage });
+  });
+
+  it('should dispatch FETCH_DATA_REQUEST and FETCH_DATA_FAILURE when response is not ok', async () => {
+    // Simula uma resposta com status 404
+    axios.get.mockResolvedValueOnce({ status: 404 });
+
+    // Despacha a ação fetchData
+    await store.dispatch(fetchData());
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: FETCH_DATA_REQUEST });
+    expect(actions[1]).toEqual({ type: FETCH_DATA_FAILURE, error: 'Network response was not ok.' });
+  });
+});
